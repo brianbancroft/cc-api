12836a2fcb3d3c89d584c0c2d4f3c5d599e4c8d1
brianbancroft <hello@brianbancroft.ca>
2022-10-16 17:52:34 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :currencies, only: [:index] 
+      get 'currencies/rate', to: 'currencies#rate'
+
+        
+          
+        
+      
+    end
+  end
 end
