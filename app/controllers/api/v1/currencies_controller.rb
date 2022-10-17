c419745fef97af5dad9e03f09043569ff4ced32f
brianbancroft <hello@brianbancroft.ca>
2022-10-16 18:17:49 -0700

    Cleanup

--- app/controllers/api/v1/currencies_controller.rb
+++ app/controllers/api/v1/currencies_controller.rb
@@ -3,8 +3,6 @@ class  Api::V1::CurrenciesController < ApplicationController
         render :json => {currencies: CurrencyList.retrieve}
     end
 
-
-    # http://localhost:3000/api/v1/currencies/rate?from=AAA&to=BBB
     def rate
         from = params[:from]
         to = params[:to]
@@ -12,8 +10,6 @@ class  Api::V1::CurrenciesController < ApplicationController
         if (from.nil? && to.nil?) || (from.length != 3 || to.length != 3) 
             render json: {message: 'bad request'}, status: :bad_request
         else 
-            response_structure = {from: 'AAA', to: 'BBB', rate: 1.23}
-
             rate = CurrencyRate.convert(from, to)
 
             render :json => {from: from, to: to, rate: rate}
